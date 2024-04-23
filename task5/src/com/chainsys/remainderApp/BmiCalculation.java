c00c974b8579faa8f9f2bd4b870e14b3155ed03c
mano3578 <mano3578@CSL0693.global.chainsys.com>
2024-04-23 17:20:54 +0530

    task5

---
+++ task5/src/com/chainsys/remainderApp/BmiCalculation.java
@@ -0,0 +1,39 @@
+package com.chainsys.remainderApp;
+
+public class BmiCalculation {
+String name;
+double height;
+int weight;
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public double getHeight() {
+	return height;
+}
+public void setHeight(double height) {
+	this.height = height;
+}
+public int getWeight() {
+	return weight;
+}
+public void setWeight(int weight) {
+	this.weight = weight;
+}
+public BmiCalculation(String name, double height, int weight) {
+	super();
+	this.name = name;
+	this.height = height;
+	this.weight = weight;
+}
+public BmiCalculation() {
+	
+}
+
+public String toString() {
+	return "BmiCalculation [name=" + name + ", height=" + height + ", weight=" + weight + "]";
+}
+
+}
---
+++ task5/src/com/chainsys/remainderApp/Validation.java
@@ -0,0 +1,69 @@
+package com.chainsys.remainderApp;
+
+import java.util.regex.Pattern;
+
+public class Validation {
+	public  boolean inputchecker(int n)
+	{
+		while(true){
+		
+			if(n<0) 
+			{
+				System.out.println("Error!Negative number detected.Please Enter a Valid input:");
+				return false;
+			}
+			return true;
+		}
+	}
+		
+		
+		 public boolean numberchecker(String s) {
+			    while (true) {
+			        try {
+			            Integer.parseInt(s);
+			            System.out.print("Don't use numbers, please enter a valid input:");
+			            return false;
+			        } catch (NumberFormatException e) {
+			            return true;
+			        }
+			    }
+			}
+		 public boolean inputchecker1(double n) {
+			    while (true) {
+			        if (n < 0) {
+			            System.out.print("Error! Negative number detected. Cannot proceed. Please enter a valid amount:");
+			            return false;
+			        } else {
+			            return true;
+			        }
+			    }
+			}
+		 public static boolean validateString(String input) {
+			 while(true) {
+		        String regex = "^[a-zA-Z ]+$";
+		        Pattern pattern = Pattern.compile(regex);       
+		        if (!pattern.matcher(input).matches()) {
+		            System.out.print("Error: Input should contain only letters:");
+		            return false;
+		        } else {
+		            return true;
+		        }
+		    }
+			 
+		 }
+		 public static boolean validateString1(String input) {
+			 while(true) {
+		        String regex = "^[a-zA-Z0-9]+$";
+		        Pattern pattern = Pattern.compile(regex);       
+		        if (!pattern.matcher(input).matches()) {
+		            System.out.print("Error: Input should contain only letters and numbers:");
+		            return false;
+		        } else {
+		            return true;
+		        }
+		    }
+		 }
+
+		 
+		 
+}
